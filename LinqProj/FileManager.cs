805653e3f58e6e9cfd69a9848c74cbf8ad771868
ivanternovyi <ivanternovyi@gmail.com>
2018-05-20 23:16:35 +0300

    Added files for IO

---
+++ LinqProj/FileManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqProj
+{
+    internal static class FileManager
+    {
+        public static string Parse(string filename)
+        {
+            string data;
+            try
+            {
+                data = System.IO.File.ReadAllText($"../{filename}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            return data;
+        }
+
+        public static void SaveToFile(string filename, IEnumerable<string> data)
+        {
+            System.IO.File.WriteAllText($"../{filename}", string.Join(",", data));
+        }
+        
+        public static void SaveToFile(string filename, IEnumerable<int> data)
+        {
+            System.IO.File.WriteAllText($"../{filename}", string.Join(",", data));
+        }
+    }
+}
--- LinqProj/Program.cs
+++ LinqProj/Program.cs
@@ -5,12 +5,12 @@ using System.Linq;
 
 namespace LinqProj
 {
-    
-    internal class Program
+    internal static class Program
     {
         
         public static void Main(string[] args)
         {
+           
             while (true)
             {
                 Console.Write("\nSelect task('q' to exit):\n1 - LinqBegin16\n2 - LinqBegin17\n3 - " +
@@ -37,84 +37,71 @@ namespace LinqProj
                     default: Console.WriteLine("BAD INPUT! PLEASE RETRY!");
                         continue;
                 }
+                Console.WriteLine("Result is successfuly written to the file.");
             }
+                
         }
+
+        private static IEnumerable<int> ConvertToInts(string data)
+        {
+            return data.Split(',').Select(int.Parse).ToArray();
+        }
+        
 
         private static void LinqBegin16()
         {
-            int[] arr = { 1, 5, -3, -6, 7, -12, 5, 10 };
-            IEnumerable<int> res = arr.Where(i => i > 0);
-            foreach (var elem in res)
-            {
-                Console.Write($"{elem} ");
-            }
+            var arr = ConvertToInts(FileManager.Parse("../16.txt"));
+            var res = arr.Where(i => i > 0);
+            FileManager.SaveToFile("../16res.txt", res);
         }
         
         private static void LinqBegin17()
         {
-            int[] arr = { 1, 2, 5, 2, 5, 9 ,2, 1 };
-            IEnumerable<int> res = arr.Where(i => i % 2 != 0).Distinct();
-            foreach (var elem in res)
-            {
-                Console.Write($"{elem} ");
-            }
+            var arr = ConvertToInts(FileManager.Parse("../17.txt"));
+            var res = arr.Where(i => i % 2 != 0).Distinct();
+            FileManager.SaveToFile("../17res.txt", res);
         }
         
         private static void LinqBegin18()
         {
-            int[] arr = { 1, 51, 12, -3, -6, 7, -12, 52, 71, 10 };
+            var arr = ConvertToInts(FileManager.Parse("../18.txt")); 
             var res = arr.Where(i => (i > 0 && (i.ToString().Length == 2 && i.ToString()[0] != '0'))).OrderBy(i => i);
-            foreach (var elem in res)
-            {
-                Console.Write($"{elem} ");
-            }
+            FileManager.SaveToFile("../18res.txt", res);
         }
         
         private static void LinqBegin19()
         {
-            string[] arr = { "lpkas", "erhc", "rexmac", "orncs", "abcw", "poqwnmv", "yiv", "z" };
+            var arr = FileManager.Parse("../19.txt").Split(','); 
             var res = arr.OrderBy(i => i.Length).ThenBy(i => i);
-            foreach (var elem in res)
-            {
-                Console.Write($"{elem} ");
-            }
+            FileManager.SaveToFile("../19res.txt", res);
         }
 
         private static void LinqBegin20()
         {
-            int[] arr = { 1, 5, 13, -3, -6, 7, -12, 5, 10 };
+            var arr = ConvertToInts(FileManager.Parse("../20.txt"));
             var d = 3;
             var res = arr.SkipWhile(i => i <= d).Where(i => (i > 0 && i % 2 != 0)).Reverse();
-            foreach (var elem in res)
-            {
-                Console.Write($"{elem} ");
-            }
+            FileManager.SaveToFile("../20res.txt", res);
         }
 
         private static void LinqBegin44()
         {
-            int[] A = { 1, 5, 13, -3, -6, 7, -12, 5, 10 };
-            int[] B = { 5, -1, 3, -23, 68, 17, 2, -5, 9 };
-            int K1 = 3;
-            int K2 = 5;
+            var A = ConvertToInts(FileManager.Parse("../44_1.txt"));
+            var B = ConvertToInts(FileManager.Parse("../44_2.txt"));
+            var K1 = 3;
+            var K2 = 5;
             var res = A.Where(i => i > K1).Concat(B.Where(i => i < K2)).OrderBy(i => i);
-            foreach (var elem in res)
-            {
-                Console.Write($"{elem} ");
-            }
+            FileManager.SaveToFile("../44res.txt", res);
         }
 
         private static void LinqBegin45()
         {
-            string[] A = { "lpkas", "erh5c", "2rexmac", "orncs", "12ab5cw", "po523qwnmv", "yiv", "z" };
-            string[] B = { "wefg", "4tfb", "45y6mac", "ys245orncs", "a35w", "i35or4mv", "yv92", "10asz2g5" };
+            var A = FileManager.Parse("../45_1.txt").Split(',');
+            var B = FileManager.Parse("../45_2.txt").Split(',');
             var L1 = 5;
             var L2 = 4;
             var res = A.Where(i => i.Length == L1).Concat(B.Where(i => i.Length == L2)).OrderByDescending(i => i);
-            foreach (var elem in res)
-            {
-                Console.Write($"{elem} ");
-            }
+            FileManager.SaveToFile("../45res.txt", res);
         }
     }
 }
