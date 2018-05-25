39dff933bd978df459fae0c13d78989f8244e31c
ivanternovyi <ivanternovyi@gmail.com>
2018-05-25 16:45:37 +0300

    Added Serialization; Added .gitignore

--- WindowsFormsApp1/WindowsFormsApp1/Line.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Line.cs
@@ -6,12 +6,20 @@ using System.Threading.Tasks;
 
 namespace WindowsFormsApp1
 {
-    class Line
+    [Serializable]
+    public class Line
     {
         public int CoordX { get; set; }
-        public int CoordX2 { get; set; }
         public int CoordY { get; set; }
+        public int CoordX2 { get; set; }
         public int CoordY2 { get; set; }
-        Line() { }
+        public Line() { }
+        public Line(int x, int y, int x2, int y2)
+        {
+            CoordX = x;
+            CoordY = y;
+            CoordX2 = x2;
+            CoordY2 = y2;
+        }
     }
 }
---
+++ WindowsFormsApp1/WindowsFormsApp1/SerializationManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace WindowsFormsApp1
+{
+    class SerializationManager
+    {
+        public static void Serialize(string filename, List<Line> lines)
+        {
+            XmlSerializer formatter = new XmlSerializer(typeof(List<Line>));
+
+            using (FileStream fs = new FileStream(filename, FileMode.Create))
+            {
+                formatter.Serialize(fs, lines);
+            }
+        }
+
+        public static List<Line> Deserialize(string filename)
+        {
+            XmlSerializer formatter = new XmlSerializer(typeof(List<Line>));
+            var lines = new List<Line>();
+            using (FileStream fs = new FileStream(filename, FileMode.Open))
+            {
+                lines = (List<Line>)formatter.Deserialize(fs);
+            }
+            return lines;
+        }
+
+    }
+}
